5b0d9aaaa4a3fc116de10c7ae7269ae7f0da3907
KPS <sainath@ufl.edu>
2017-03-29 13:02:16 -0400

    Binary heap done

--- src/MainFunction.java
+++ src/MainFunction.java
@@ -10,21 +10,21 @@ import java.util.Map;
 
 class node{
 	int data;
-	int freq;
+	long freq;
 	node(){
 		data=0;
 		freq=0;
 	}
-	int getData(){
+	long getData(){
 		return this.data;
 	}
-	int getFreq(){
+	long getFreq(){
 		return this.freq;
 	}
 }
 class huffManTree{
-	int data;
-	int freq;
+	long data;
+	long freq;
 	huffManTree left;
 	huffManTree right;
 	huffManTree(int dat,int fre){
@@ -32,6 +32,9 @@ class huffManTree{
 		freq=fre;
 	}
 }
+
+
+
 class binaryHeap{
 	int left(int i){
 		return (2*i)+1;
@@ -64,18 +67,59 @@ class binaryHeap{
 			this.heapify(l,i,n);
 		}
 	}
+	void build_tree_using_binary_heap(ArrayList<node> l){
+		ArrayList<node> temp=new ArrayList<node>(l);
+		while(temp.size()>1){
+			node n1=this.extract_min(temp);
+			node n2=new node();
+			n2=this.extract_min(temp);
+			node n3=new node();
+			n3.data=0;
+			n3.freq=n1.freq+n2.freq;
+			temp.add(n3);
+			//System.out.println(n1.data+" "+n1.freq+" " + n2.data+" "+n2.freq);
+			//System.out.println(n3.data+" "+n3.freq);
+			this.heapify(temp, temp.size()-1, temp.size());
+		}
+		
+	}
 }
+
+
+class fourWayHeap{
+	int data;
+	long freq;
+	fourWayHeap(){
+		data=0;
+		freq=0;
+	}
+	int getData(){
+		return this.data;
+	}
+	long getFreq(){
+		return this.freq;
+	}
+	
+}
+
+
+
+
+
+
+
+
 public class MainFunction {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		
 		
-		Map<Integer, Integer> fre=new HashMap<Integer, Integer>();
+		Map<Integer, Long> fre=new HashMap<Integer, Long>();
 		FileReader fr=null;
 		try {
 			//fr = new FileReader("example.txt");
-			fr=new FileReader("/home/kps/workspace/ADSProject/src/example.txt");
+			fr=new FileReader("/home/kps/workspace/ADSProject/src/sample_input_large.txt");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -83,21 +127,18 @@ public class MainFunction {
 		ArrayList<node> l=new ArrayList<node>();
 		BufferedReader br=new BufferedReader(fr);
 		String S=new String();
-		
 		while((S=br.readLine())!=null){
 			int temp=Integer.parseInt(S);
-			if(fre.containsKey(temp)) fre.put(temp, (int)fre.get(temp)+1);
-			else fre.put(temp, 1);
-			
+			if(fre.containsKey(temp)) fre.put(temp, (long)fre.get(temp)+1);
+			else fre.put(temp, (long)1);
 		}
 		Iterator it=fre.entrySet().iterator();
 		while(it.hasNext()){
 			node n=new node();
 			Map.Entry temp=(Map.Entry)it.next();
 			n.data=(int)temp.getKey();
-			n.freq=(int)temp.getValue();
+			n.freq=(long)temp.getValue();
 			l.add(n);
-			//System.out.println(n.data+ " "+n.freq);
 			it.remove();
 		}
 		//binary heap
@@ -108,24 +149,22 @@ public class MainFunction {
 			//System.out.println(t.data+ " "+t.freq);
 			//System.out.println(l.get(i).getData()+" "+l.get(i).getFreq());
 		}
+		System.out.println("");
 		long startTime = System.currentTimeMillis();
-		while(l.size()>1){
-			node n1=bh.extract_min(l);
-			node n2=new node();
-			n2=bh.extract_min(l);
-			node n3=new node();
-			n3.data=0;
-			n3.freq=n1.freq+n2.freq;
-			l.add(n3);
-			bh.heapify(l, l.size()-1, l.size());
+		for(int i = 0; i < 10; i++){    //run 10 times on given data set 
+			bh.build_tree_using_binary_heap(l);
 		}
+		long stopTime = System.currentTimeMillis();
+		System.out.println(stopTime-startTime+" MilliSec");
+		
+		
 		for(int i=0;i<l.size();i++){
 			node t=l.get(i);
 			//System.out.println(t.data+ " "+t.freq);
-			System.out.println(l.get(i).getData()+" "+l.get(i).getFreq());
+			//System.out.println(l.get(i).getData()+" "+l.get(i).getFreq());
 		}
-		long stopTime = System.currentTimeMillis();
-		System.out.println(stopTime-startTime+"MilliSec");
+		
+		
 		
 	}
 
